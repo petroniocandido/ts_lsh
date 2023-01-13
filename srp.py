bf4db751d0a4b0fb11c03bef83811b0eede5d030
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2023-01-13 16:06:30 -0300

    Distributions on SRP

--- srp.py
+++ srp.py
@@ -4,7 +4,13 @@ class SignedRandomProjectionLSH(LSH):
   def __init__(self, **kwargs):
     super(SignedRandomProjectionLSH, self).__init__(**kwargs)
 
-    self.weights = np.random.randn(self.length)
+    scale = kwargs.get("scale", 1.0)
+
+    dist = kwargs.get('dist','normal')
+    if dist == 'normal':
+      self.weights = np.random.randn(self.length) * scale
+    elif dist == 'unif':
+      self.weights = (np.random.rand(self.length) * 2 * scale) - scale
 
   def _hashfunction(self, input : np.array, **kwargs):
     if len(input) != self.length:

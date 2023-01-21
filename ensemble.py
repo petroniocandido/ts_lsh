4b7389d8819adbee561ec8376ca0ce5e1c0163b5
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2023-01-21 15:27:21 -0300

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -7,12 +7,28 @@ class EnsembleLSH(MultipleLSH):
     
     self.aggregation = kwargs.get("aggregation", "mean")
 
-    self.owa_weights = kwargs.get("owa_weights",None)
+    self.aggregation = kwargs.get("aggregation", "srp")
+
+    self.aggregation_weights = kwargs.get("aggregation_weights",None)
+
+    if self.aggregation == "srp":
+      self.scale = kwargs.get("scale", 1.0)
+
+      self.dist = kwargs.get('dist','normal')
+      if self.dist == 'normal':
+        self.aggregation_weights = np.random.randn(self.num) * self.scale
+      elif self.dist == 'unif':
+        self.aggregation_weights = (np.random.rand(self.num) * 2 * self.scale) - self.scale
 
   def _hashfunction(self, input : np.array, **kwargs):
     hashes = super(EnsembleLSH, self)._hashfunction(input)
-    if self.aggregation == "owa" and self.owa_weigts is not None:
-      owa(hashes, self.owa_weights)
+    if self.aggregation == "owa":
+      if self.aggregation_weights is None:
+        raise Exception("Ordered Weight Aggregation weights are not set!")
+      return owa(hashes, self.aggregation_weights)
+    elif self.aggregation == "srp":
+      return np.dot(self.aggregation_weights, hashes)
     else:
-      w = get_owa_weights(self.aggregation,input)
-      return owa(input, w)
+      if self.aggregation_weights is None:
+        self.aggregation_weights = get_owa_weights(self.aggregation,input)
+      return owa(hashes, self.aggregation_weights)

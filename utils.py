563415c038fd55d350a4a115707c7b688524236e
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2023-04-09 08:47:46 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,9 +18,15 @@ def distance_distribution(mat, nbins=20):
   ax.set_title("Distance Distribution")
 
   
-def data_vs_hash_distance_distribution(dataset : np.array, lsh, fn_distance = euclidean, nbins=20, mat=None, mat_hash=None):
+def data_vs_hash_distance_distribution(**kwargs):
+  mat : np.array = kwargs.get('mat', None)
+  mat_hash : np.array = kwargs.get('mat_hash', None)
   
   if mat is None and mat_hash is None:
+    dataset : np.array = kwargs.get('dataset',None)
+    lsh = kwargs.get('lsh',None)
+    fn_distance = kwargs.get('fn_distance', euclidean)
+    nbins = kwargs.get('nbins', 20)
     mat, mat_hash = data_vs_hash_distance_matrices(dataset, lsh, fn_distance, nbins)
   
   fig = plt.figure(layout='constrained', figsize=(10, 5))
@@ -45,9 +51,16 @@ def data_vs_hash_distance_distribution(dataset : np.array, lsh, fn_distance = eu
 
 
 def conditional_distance_distribution(dataset : np.array, lsh, fn_distance = euclidean, nbins=20, mat=None, mat_hash=None):
+  mat : np.array = kwargs.get('mat', None)
+  mat_hash : np.array = kwargs.get('mat_hash', None)
+  
   if mat is None and mat_hash is None:
+    dataset : np.array = kwargs.get('dataset',None)
+    lsh = kwargs.get('lsh',None)
+    fn_distance = kwargs.get('fn_distance', euclidean)
+    nbins = kwargs.get('nbins', 20)
     mat, mat_hash = data_vs_hash_distance_matrices(dataset, lsh, fn_distance, nbins)
-
+  
   bins, freqs = matrix_histogram(mat, nbins)
 
   dist = {bin : [] for bin in bins}

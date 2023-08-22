71e05cb2daf0e3283284cc6afdaaee1559d64bb9
Petrônio Cândido de Lima e Silva <petronio.candido@gmail.com>
2023-08-21 22:56:51 -0300

    Introducing gram_schmidt orthonormalization

--- common.py
+++ common.py
@@ -1,6 +1,24 @@
 import math
 import numpy as np
 
+def gram_schmidt(A):
+  # From: https://zerobone.net/blog/cs/gram-schmidt-orthogonalization/
+  (n, m) = A.shape
+  for i in range(m):        
+    q = A[:, i] # i-th column of A
+        
+    for j in range(i):
+      q = q - np.dot(A[:, j], A[:, i]) * A[:, j]
+        
+    if np.array_equal(q, np.zeros(q.shape)):
+      raise np.linalg.LinAlgError("The column vectors are not linearly independent")
+        
+    # normalize q
+    q = q / np.sqrt(np.dot(q, q))
+        
+    # write the vector back in the matrix
+    A[:, i] = q
+
 def sigmoid(x):
   return 1/(1 + np.exp(-x))
 
